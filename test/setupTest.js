dd1c6381c03421c71b7cd98e017511d23e35ca6e
Eunho Lee <55861805+devethan@users.noreply.github.com>
2020-06-25 22:32:34 +0900

    [Enhance] Update provider to reset relay env (#144)
    * Update provider to reset relay env
    * Push codes before resolving test error
    * Mock relay.init to pass tests
    
    Co-authored-by: hyochan <dooboolab@gmail.com>

--- test/setupTest.js
+++ test/setupTest.js
@@ -1,8 +1,10 @@
 import 'jest-styled-components';
 import '@testing-library/jest-native/extend-expect';
 
-jest.mock('../src/relay/RelayEnvironment', () => {
+jest.mock('../src/relay', () => {
   // eslint-disable-next-line
   const { createMockEnvironment } = require('relay-test-utils');
-  return createMockEnvironment();
+  const mockRelay = createMockEnvironment();
+  mockRelay.init = jest.fn();
+  return mockRelay;
 });
